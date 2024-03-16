433ffadbf1cb203cc0e9e132e9e3194c3a52b673
LuanRoch <43256732+LuanRoch@users.noreply.github.com>
2024-03-16 00:37:07 -0300

    Add files via upload

---
+++ javascript/login.js
@@ -0,0 +1,14 @@
+function logar() {
+    var login = document.getElementById('login').value
+    var senha = document.getElementById('senha').value
+
+
+    if (login == "pipoca" && senha == "19012024") {
+        alert('Vale qualquer coisa!!! qualquer coisa menos aquilo ;)');
+        location.href = "home.html"
+    } else {
+        alert('Ta me devendo um BEIJO')
+    }
+
+
+}
